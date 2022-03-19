55c06536f4f6883888d026174b8e589e869e7b6f
Afri02ansyah <afriansyah1109@gmail.com>
2022-03-19 09:44:11 +0700

    Update java dan manifest

---
+++ app/src/main/java/com/example/afriproject/MenuActivity.java
@@ -0,0 +1,24 @@
+package com.example.afriproject;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MenuActivity extends AppCompatActivity {
+    ImageView ivMenu;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ivMenu = findViewById(R.id.iv_menu);
+        ivMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent( MenuActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
